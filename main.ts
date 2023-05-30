1d5f02aa8cd623e62dd39ad5554fa7bfb0fe75cd
manuelcasas <128051762+manuelcasas23@users.noreply.github.com>
2023-05-30 17:41:42 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,27 @@
-let strip: neopixel.Strip = null
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 10) {
-        strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+        maqueen.motorStop(maqueen.Motors.All)
         strip.showColor(neopixel.colors(NeoPixelColors.Orange))
-        maqueen.motorStop(maqueen.Motors.All)
-    } else if (IR.IR_read() == 19) {
-        strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
-        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    } else if (IR.IR_read() == 16) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
-    } else {
-        strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    } else if (IR.IR_read() == 27) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 100)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    } else if (IR.IR_read() == 31) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    } else if (IR.IR_read() == 10) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    } else if (IR.IR_read() == 8) {
         maqueen.motorStop(maqueen.Motors.All)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (IR.IR_read() == 29) {
+        music.playTone(262, music.beat(BeatFraction.Eighth))
     }
 })
 basic.forever(function () {
